a46605660cc69810cc1d678cdd64b8b0f45c3d57
ian123an <64454799+ian123an@users.noreply.github.com>
2021-03-02 17:33:08 +0800

    Create 9-6 Score.h

---
+++ 9-6 Score.h
@@ -0,0 +1,20 @@
+#ifndef SCORE_H
+#define SCORE_H
+#include<string>
+using namespace std;
+
+class Score
+{
+public:
+	Score();
+	Score(string,int);
+	void setName(string);
+	void setMath(int);
+	string getName();
+	int getMath();
+	void print();
+private:
+	string name;
+	int math;
+};
+#endif
