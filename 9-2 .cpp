9673cf72993593e823ed321ce01299a842f1c545
ian123an <64454799+ian123an@users.noreply.github.com>
2021-03-02 16:25:37 +0800

    Create 9-2 .cpp

---
+++ 9-2 .cpp
@@ -0,0 +1,21 @@
+#include"Rectangle.h"
+
+	double width, height;
+	Rectangle::Rectangle()
+	{
+		width = 1;
+		height = 1;
+	}
+	Rectangle::Rectangle(double newWidth, double newHeight)
+	{
+		width = newWidth;
+		height = newHeight;
+	}
+	double Rectangle::getArea()
+	{
+		return width * height;
+	}
+	double Rectangle::getPerimeter()
+	{
+		return (width + height) * 2;
+	}
