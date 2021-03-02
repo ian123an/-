046819be1bbbb4a58400287b362a0be721c85131
ian123an <64454799+ian123an@users.noreply.github.com>
2021-03-02 17:33:28 +0800

    Create 9-6 Score.cpp

---
+++ 9-6 Score.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include"Score.h"
+
+Score::Score()
+{}
+Score::Score(string newName,int newMath)
+{
+	name = newName;
+	math = newMath;
+}
+void Score::setName(string n)
+{
+	name = n;
+}
+void Score::setMath(int m)
+{
+	math = m;
+}
+string Score::getName()
+{
+	return name;
+}
+int Score::getMath()
+{
+	return math;
+}
+void Score::print()
+{
+	cout << getName() << " " << getMath() << endl;
+}
