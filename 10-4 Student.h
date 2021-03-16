925a5ead236ce632a723f0d1e86559c3243fbd3a
ian123an <64454799+ian123an@users.noreply.github.com>
2021-03-16 18:03:13 +0800

    Create 10-4 Student.h

---
+++ 10-4 Student.h
@@ -0,0 +1,24 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include<string>
+#include"Date.h"
+using namespace std;
+
+class Student
+{
+private:
+	string name;
+	Date birthDay;
+	int score;
+public:
+	Student(string, Date, int);
+	void setName(string);
+	void setDate(Date);
+	void setScore(int);
+	string getName() const;
+	Date getDate() const;
+	int getScore() const;
+	void print();
+
+};
+#endif
