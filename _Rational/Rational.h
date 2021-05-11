1eced91c7fdb8b5fe5d0e961f3b7c010566220cd
ian123an <64454799+ian123an@users.noreply.github.com>
2021-05-11 16:07:34 +0800

    Create Rational.h

---
+++ _Rational/Rational.h
@@ -0,0 +1,62 @@
+#ifndef RATIONAL_H
+#define RATIONAL_H
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Rational
+{
+public:
+	Rational();
+	Rational(int numerator, int denominator);
+	int getNumerator() const;
+	int getDenominator() const;
+	Rational add(const Rational& secondRational) const;
+	Rational subtract(const Rational& secondRational) const;
+	Rational multiply(const Rational& secondRational) const;
+	Rational divide(const Rational& secondRational) const;
+	int compareTo(const Rational& secondRational) const;
+	bool equals(const Rational& secondRational) const;
+	int intValue() const;
+	double doubleValue() const;
+	string toString() const;
+
+	Rational(int numerator);
+
+	Rational& operator+=(const Rational& secondRational);
+	Rational& operator-=(const Rational& secondRational);
+	Rational& operator*=(const Rational& secondRational);
+	Rational& operator/=(const Rational& secondRational);
+
+	int& operator[](int index);
+
+	Rational& operator++();
+	Rational& operator--();
+
+	Rational operator++(int dummy);
+	Rational operator--(int dummy);
+
+	Rational operator+();
+	Rational operator-();
+
+	friend ostream& operator<<(ostream&, const Rational&);
+	friend istream& operator>>(istream&, Rational&);
+
+private:
+	int numerator;
+	int denominator;
+	static int gcd(int n, int d);
+};
+bool operator<(const Rational& r1, const Rational& r2);
+bool operator<=(const Rational& r1, const Rational& r2);
+bool operator>(const Rational& r1, const Rational& r2);
+bool operator>=(const Rational& r1, const Rational& r2);
+bool operator==(const Rational& r1, const Rational& r2);
+bool operator!=(const Rational& r1, const Rational& r2);
+
+Rational operator+(const Rational& r1, const Rational& r2);
+Rational operator-(const Rational& r1, const Rational& r2);
+Rational operator*(const Rational& r1, const Rational& r2);
+Rational operator/(const Rational& r1, const Rational& r2);
+
+#endif
