e1ebe00af42eab8cbb7791a6758912edd604da96
ian123an <64454799+ian123an@users.noreply.github.com>
2021-06-08 17:04:24 +0800

    Create sphere.h

---
+++ c++多型/sphere.h
@@ -0,0 +1,19 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+#include <string>
+#include "Shape.h"
+using namespace std;
+class Sphere : public Shape
+{
+private:
+	double radius;
+public:
+	Sphere();
+	Sphere(double,string);
+	double getRadius();
+	void setRadius(double);
+	double getVolume();
+	string toString();
+	
+};
+#endif
