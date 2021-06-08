d0319f9467eb41d0f3e6948a609fdc273b71e4d6
ian123an <64454799+ian123an@users.noreply.github.com>
2021-06-08 16:55:15 +0800

    Create cuboid.cpp

---
+++ C++繼承/cuboid.cpp
@@ -0,0 +1,41 @@
+#include "Cuboid.h"
+
+Cuboid::Cuboid(double _l,double _w,double _h, string _c)
+{
+	length = _l;
+	width = _w;
+	height = _h;
+	setColor(_c);
+}
+double Cuboid::getLength()
+{
+	return length;
+}
+double Cuboid::getWidth()
+{
+	return width;
+}
+double Cuboid::getHeight()
+{
+	return height;
+}
+void Cuboid::setLength(double _l)
+{
+	length = _l;
+}
+void Cuboid::setWidth(double _w)
+{
+	width = _w;
+}
+void Cuboid::setHeight(double _h)
+{
+	height = _h;
+}
+double Cuboid::getVolume()
+{
+	return length * width * height;
+}
+string Cuboid::toString()
+{
+	return "Cuboid";
+}
